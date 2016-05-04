52a6e774c29a9d5a2d89d421e6e93bc9b1f04e30
Carla Sarappa <carla.sarappa@gmail.com>
2016-05-04 17:29:48 -0300

    continuacion farolas

--- src/farolas/Farola.java
+++ src/farolas/Farola.java
@@ -1,15 +1,41 @@
 package farolas;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Farola {
 	
 	Boolean prendida = false;
+	List<Farola> vecinas = new ArrayList<Farola>();
 	
 	public void prender() {
-		
+		if(!prendida) {
+			prendida = true;
+			for(Farola farola : vecinas){
+				farola.prender();
+			}
+		} 
+	}
+	
+	public void apagar() {
+		prendida = false;
+	}
+
+	public List<Farola> getVecinas() {
+		return vecinas;
+	}
+
+	public void setVecinas(List<Farola> vecinas) {
+		this.vecinas = vecinas;
 	}
 
 	public boolean isPrendida() {
-		return true;
+		return prendida;
+	}
+	
+	public void conectarCon(Farola farola){
+		vecinas.add(farola);
+		
 	}
 
 }
--- test/farolas/FarolasTest.java
+++ test/farolas/FarolasTest.java
@@ -2,6 +2,9 @@ package farolas;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
 public class FarolasTest {
@@ -20,4 +23,65 @@ public class FarolasTest {
 		assertFalse(farola.isPrendida());	
 	}
 	
+	@Test
+	public void testCuandoAUnaFarolaPrendidaSeLeDicePrenderSiguePrendida(){
+		Farola farola = new Farola();
+		farola.prender();
+		farola.prender();
+		assertTrue(farola.isPrendida());	
+	}
+	
+	@Test
+	public void testCuandoAUnaFarolaApagadaSeLeDiceApagarSigueApagada(){
+		Farola farola = new Farola();
+		farola.apagar();
+		farola.apagar();
+		assertFalse(farola.isPrendida());
+	}
+	
+	@Test
+	public void testLasFarolasPuedenEstarConectadasConOtrasFarolas(){
+		Farola farola = new Farola();
+		Farola otraFarola = new Farola();
+		Farola tercerFarola = new Farola();
+		farola.conectarCon(otraFarola);
+		farola.conectarCon(tercerFarola);
+		
+		List<Farola> listas = new ArrayList<Farola>();
+		listas.add(otraFarola);
+		listas.add(tercerFarola);
+		
+		assertArrayEquals(listas.toArray(), farola.getVecinas().toArray());
+	}
+
+	@Test
+	public void testAlPrenderUnaFarolaSeDebenPrenderTodasLasDemas(){
+		Farola farola = new Farola();
+		Farola otraFarola = new Farola();
+		Farola tercerFarola = new Farola();
+		farola.conectarCon(otraFarola);
+		farola.conectarCon(tercerFarola);
+		otraFarola.conectarCon(tercerFarola);
+		tercerFarola.conectarCon(farola);
+		farola.prender();
+		
+		assertTrue(otraFarola.isPrendida());
+		assertTrue(tercerFarola.isPrendida());
+		
+	}
+	
+	@Test
+	public void testAlApagarUnaFarolaSeDebenApagarTodasLasDemas(){
+		Farola farola = new Farola();
+		Farola otraFarola = new Farola();
+		Farola tercerFarola = new Farola();
+		farola.conectarCon(otraFarola);
+		farola.conectarCon(tercerFarola);
+		otraFarola.conectarCon(tercerFarola);
+		tercerFarola.conectarCon(farola);
+		farola.apagar();
+		
+		assertTrue(otraFarola.isPrendida());
+		assertTrue(tercerFarola.isPrendida());
+	}
 }
